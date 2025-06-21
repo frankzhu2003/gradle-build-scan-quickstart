242e7a0d7a8d7f9f6bb415c384702485b114a59a
Frank Zhu <70764560+frankzhu2003@users.noreply.github.com>
2025-06-21 14:46:31 -0400

    Update build.gradle.kts
    Signed-off-by: Frank Zhu <70764560+frankzhu2003@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,13 @@ plugins {
 }
 
 repositories {
-    https://repo.spring.io/snapshot
+    google()
     mavenCentral()
 }
 
 dependencies {
     implementation("org.apache.logging.log4j:log4j-core:2.15.0")
-    implementation("org.springframework.data:spring-data-bom:2025.1.0-20250618.082622-1090") 
+    implementation("com.google.guava:guava:33.0.0-jre") 
     testImplementation(platform("org.junit:junit-bom:5.13.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
