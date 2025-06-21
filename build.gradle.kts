4114efc2ca1618dae7f4221d61738df0ef56326d
Frank Zhu <70764560+frankzhu2003@users.noreply.github.com>
2025-06-21 15:00:53 -0400

    Update build.gradle.kts
    Signed-off-by: Frank Zhu <70764560+frankzhu2003@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 
 dependencies {
     implementation("org.apache.logging.log4j:log4j-core:2.15.0")
-    implementation("androidx.annotation:annotation:1.8.1")
+    implementation("androidx.core:core-viewtree:1.0.0")
     testImplementation(platform("org.junit:junit-bom:5.13.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
