ab54b107d4a51787560d2a244471a187395d406b
Frank Zhu <70764560+frankzhu2003@users.noreply.github.com>
2025-06-22 14:47:54 -0400

    Update build.gradle.kts
    Signed-off-by: Frank Zhu <70764560+frankzhu2003@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,21 @@ repositories {
     mavenCentral()
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17)) // Set your desired Java version
+    }
+}
+
 dependencies {
+
     implementation("org.apache.logging.log4j:log4j-core:2.15.0")
+
     implementation("androidx.collection:collection:1.4.2")
+
+    implementation("org.projectlombok:lombok:1.18.38")
+
+
     testImplementation(platform("org.junit:junit-bom:5.13.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
